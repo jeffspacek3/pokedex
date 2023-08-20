cba7e5534d4a100100658b9db5eef7de099350a7
Jeff Spacek <Jeffrey.spacek@gmail.com>
2023-08-19 21:57:53 -0700

    yuip
    up

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
 alert('hello world') ;
 
+<<<<<<< Updated upstream
   let favoriteFood='Pizza' ;
 document.write(favoriteFood) ;
+=======
+
+document.write(favoriteFood)
+letfavoriteFood='lasagna'
+>>>>>>> Stashed changes
