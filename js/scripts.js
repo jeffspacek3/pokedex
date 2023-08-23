ac89d662d978d6e849d8de48c4d426b5c8231d63
Jeff Spacek <33886504+jeffspacek3@users.noreply.github.com>
2023-08-22 21:51:55 -0700

    Update scripts.js
    exercise 1.2

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,18 @@
-
+let pokemonList = [];
+pokemonList = [
+    {
+        name: 'Bulbasaur',
+        type: ['Grass', 'Poison'],
+        height: 0.11
+    },
+    {
+        name: 'Charmander',
+        type: ['Fire'],
+        height: 0.12
+    },
+    {
+        name: 'Pikachu',
+        type: ['Electric'],
+        height: 0.13
+    }
+]
