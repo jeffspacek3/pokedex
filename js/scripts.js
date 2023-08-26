aa8c89fd948eedeeb6df7ba44b0465b8f030b089
Jeff Spacek <33886504+jeffspacek3@users.noreply.github.com>
2023-08-26 08:51:48 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,22 +3,22 @@ pokemonList = [
     {
         name: 'Bulbasaur',
         type: ['Grass', 'Poison'],
-        (height: 23)
+        height: 23
     },
     {
         name: 'Charmander',
         type: ['Fire'],
-        (height: 15)
+        height: 15
     },
     {
         name: 'Pikachu',
         type: ['Electric'],
-        (height: 9)
+        height: 9
     }
 ]
 
 for (let i=0; i < pokemonList.length; i++){
-    if (pokemonList[i].height <19 && pokemonList[i].age >10){
+    if (pokemonList[i].height <19 && pokemonList[i].height >10){
         document.write(pokemonList[i].height + "is an average fighter");
     }else if (pokemonList[i].height <9){
         document.write(pokemonList[i].height + "is an small fighter");
