3ed188058ee764efeae410a3df045c30fa48813e
Jeff Spacek <33886504+jeffspacek3@users.noreply.github.com>
2023-08-25 22:42:50 -0700

    Update scripts.js
    1.3 javascript assingment first task

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,31 @@ pokemonList = [
     {
         name: 'Bulbasaur',
         type: ['Grass', 'Poison'],
-        height: 0.11
+        height: 23
     },
     {
         name: 'Charmander',
         type: ['Fire'],
-        height: 0.12
+        height: 15
     },
     {
         name: 'Pikachu',
         type: ['Electric'],
-        height: 0.13
+        height: 09
     }
 ]
+
+for (let i=0; i < pokemonList.length; i++){
+    if (pokemonList[i].height <19 && pokemonList[i].age >10){
+        console.log(pokemonList[i].height + " is an average fighter");
+    }else if (pokemonList[i].height <9){
+        console.log(pokemonList[i].height + "is an small fighter");
+    }else {
+        console.log(pokemonList[i].height + "is a tall fighter");
+    }
+}
+
+
+
+
+    
