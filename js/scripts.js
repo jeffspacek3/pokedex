c360bd34a12ac59b313986800847a2b0706fa917
Jeff Spacek <33886504+jeffspacek3@users.noreply.github.com>
2023-08-27 21:28:51 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -22,6 +22,6 @@ for (let i=0; i < pokemonList.length; i++){
     }else if (pokemonList[i].height <9){
         document.write(pokemonList[i].name + "(" + pokemonList[i].height + ")" + "is an small fighter");
     }else if (pokemonList[i].height >20){
-        document.write(pokemonList[i].name + "(" pokemonList[i].height + ")" + "is a tall fighter");
+        document.write(pokemonList[i].name + "(" + pokemonList[i].height + ")" + "is a tall fighter");
     }
 }
