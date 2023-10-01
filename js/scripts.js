9a685a199cc60383c83c94bddecad3d3a963d4af
Jeff Spacek <33886504+jeffspacek3@users.noreply.github.com>
2023-09-30 22:00:40 -0700

    Update scripts.js
    including more code regarding the modal

--- js/scripts.js
+++ js/scripts.js
@@ -68,3 +68,39 @@ function showModal() {
 document.querySelector('#show-modal').addEventListener('click', () => {
   showModal();
 });
+
+function showModal(title, text) {
+  let modalContainer = document.querySelector('#modal-container');
+
+  // Clear all existing modal content
+  modalContainer.innerHTML = '';
+
+  let modal = document.createElement('div');
+  modal.classList.add('modal');
+
+  // Add the new modal content
+  let closeButtonElement = document.createElement('button');
+  closeButtonElement.classList.add('modal-close');
+  closeButtonElement.innerText = 'Close';
+
+  let titleElement = document.createElement('h1');
+  titleElement.innerText = title;
+
+  let contentElement = document.createElement('p');
+  contentElement.innerText = text;
+
+  modal.appendChild(closeButtonElement);
+  modal.appendChild(titleElement);
+  modal.appendChild(contentElement);
+  modalContainer.appendChild(modal);
+
+  modalContainer.classList.add('is-visible');
+}
+
+document.querySelector('#show-modal').addEventListener('click', () => {
+  showModal('Modal title', 'This is the modal content!');
+});
+
+
+//--notes---//
+
