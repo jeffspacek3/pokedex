eb835f005f77a9bf699694204e9bddf7ed895e25
Jeff Spacek <33886504+jeffspacek3@users.noreply.github.com>
2023-10-01 22:08:53 -0700

    Update scripts.js
    dialog and adevent listener

--- js/scripts.js
+++ js/scripts.js
@@ -132,6 +132,37 @@ modalContainer.addEventListener('click', (e) => {
   }
 });
 
+//---dialog---//
+function showDialog(title, text) {
+  showModal(title, text);
+
+  // We have defined modalContainer here
+  let modalContainer = document.querySelector('#modal-container');
+
+  // We want to add a confirm and cancel button to the modal
+  let modal = modalContainer.querySelector('.modal');
+
+  let confirmButton = document.createElement('button');
+  confirmButton.classList.add('modal-confirm');
+  confirmButton.innerText = 'Confirm';
+
+  let cancelButton = document.createElement('button');
+  cancelButton.classList.add('modal-cancel');
+  cancelButton.innerText = 'Cancel';
+
+  modal.appendChild(confirmButton);
+  modal.appendChild(cancelButton);
+
+  // We want to focus the confirmButton so that the user can simply press Enter
+  confirmButton.focus();
+}
+
+
+//---event---//
+document.querySelector('#show-dialog').addEventListener('click', () => {
+  showDialog('Confirm action', 'Are you sure you want to do this?');
+});
+
 
 //--notes---//
 
