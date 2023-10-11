ff05de4560ef92fda5df76277ca07a32552d3b0b
Jeff Spacek <33886504+jeffspacek3@users.noreply.github.com>
2023-10-10 17:47:09 -0700

    Update scripts.js
    code clean up. code that was not used for any particular reason. the old code was deleted

--- js/scripts.js
+++ js/scripts.js
@@ -60,16 +60,6 @@ pokemonRepository.loadList().then(function () {
     });
 });
 
-//---modal
-function showModal() {
-  let modalContainer = document.querySelector('#modal-container');
-  modalContainer.classList.add('is-visible');
-}
-
-document.querySelector('#show-modal').addEventListener('click', () => {
-  showModal();
-});
-
 function showModal(title, text) {
   let modalContainer = document.querySelector('#modal-container');
   // Clear all existing modal content
